687818a441e2a04362b5b21cc6a1528a415f9e77
DianaDuk <dianadukelska@gmail.com>
2024-10-23 20:17:47 +0300

    dz26
    use hooks

--- my-app/src/components/EmojiList.js
+++ my-app/src/components/EmojiList.js
@@ -1,12 +1,14 @@
-export function EmojiList({ emojis, votes, onVote }) {
+import React, { memo } from 'react';
+
+export const EmojiList = memo(function EmojiList({ emojis, votes, onVote }) {
   return (
-    <div className="emoji-container">
-      {emojis.map((emoji, index) => (
-        <div key={index} className="emoji-item" onClick={() => onVote(index)}>
-          <span className="emoji">{emoji}</span>
-          <span className="vote-count">{votes[index]}</span>
-        </div>
-      ))}
-    </div>
+      <div className="emoji-container">
+          {emojis.map((emoji, index) => (
+              <div key={index} className="emoji-item" onClick={() => onVote(index)}>
+                  <span className="emoji">{emoji}</span>
+                  <span className="vote-count">{votes[index]}</span>
+              </div>
+          ))}
+      </div>
   );
-}
+});
--- my-app/src/components/Result.js
+++ my-app/src/components/Result.js
@@ -1,12 +1,14 @@
-export function Results({ votes, emojis }) {
-  const maxVotes = Math.max(...votes);
-  const winnerIndex = votes.indexOf(maxVotes);
+import React, { memo, useMemo } from 'react';
+
+export const Results = memo(function Results({ votes, emojis }) {
+  const maxVotes = useMemo(() => Math.max(...votes), [votes]);
+  const winnerIndex = useMemo(() => votes.indexOf(maxVotes), [votes, maxVotes]);
 
   return (
-    <div className="results">
-      <h2>Переможець:</h2>
-      <div className="winner-emoji">{emojis[winnerIndex]}</div>
-      <p>Кількість голосів: {maxVotes}</p>
-    </div>
+      <div className="results">
+          <h2>Переможець:</h2>
+          <div className="winner-emoji">{emojis[winnerIndex]}</div>
+          <p>Кількість голосів: {maxVotes}</p>
+      </div>
   );
-}
+});
